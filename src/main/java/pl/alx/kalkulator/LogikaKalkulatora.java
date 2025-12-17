5e99729b4d56b0c9712849b468fac6dae87bf860
daniel <dncoursetmp@pm.me>
2025-12-17 14:14:45 +0100

    Dzielenie

--- src/main/java/pl/alx/kalkulator/LogikaKalkulatora.java
+++ src/main/java/pl/alx/kalkulator/LogikaKalkulatora.java
@@ -1,9 +1,15 @@
 package pl.alx.kalkulator;
 
 public class LogikaKalkulatora {
+	private long dziel(long liczba1, long liczba2) {
+		return switch(liczba) {
+			case 0 -> throw new IllegalArgumentException("Dzielenie przez zero");
+			default -> liczba1 / liczba2;
+		}
+	}
 
     public String[] dostepneOperacje() {
-        return new String[] { "+", "-", "*" };
+        return new String[] { "+", "-", "*", "/" };
     }
 
     public long oblicz(long liczba1, long liczba2, String operacja) {
@@ -11,6 +17,7 @@ public class LogikaKalkulatora {
             case "+" -> liczba1 + liczba2;
             case "-" -> liczba1 - liczba2;
             case "*" -> liczba1 * liczba2;
+            case "/" -> liczba1 / liczba2;
             default -> throw new IllegalArgumentException("Nieznana operacja " + operacja);
         };
     }
